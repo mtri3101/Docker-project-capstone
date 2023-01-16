ab2ee32c26738236b1f57c2f28df49281548d642
mtri3101 <lamtangminhtri3101@gmail.com>
2023-01-16 16:11:31 +0700

    up project

--- src/routers/v1/index.js
+++ src/routers/v1/index.js
@@ -10,28 +10,28 @@ const v1 = express.Router();
 
 //Register, Login
 v1.post("/users/register", userController.createUser());
-v1.post("/login", authController.login());
+v1.post("/users/login", authController.login());
 
 //Page Home
-v1.get("/images", authorization, imageController.getImages());
-v1.get("/images/name/:name", authorization, imageController.getImagesByName());
+v1.get("/images/getImages", authorization, imageController.getImages());
+v1.get("/images/getImagesByName/:name", authorization, imageController.getImagesByName());
 
 //Page Chi tiết
-v1.get("/images/:imageId", authorization, imageController.getImageById())
-v1.get("/images/comments/:imageId", authorization, imageController.getImageCommentById())
-v1.post("/images/save/:imageId", authorization, imageController.saveImage())
-v1.get("/images/save/:imageId", authorization, imageController.getImageSavedById())
-v1.post("/images/comments/:imageId", authorization, imageController.commentImage())
+v1.get("/images/getImagesById/:imageId", authorization, imageController.getImageById())
+v1.get("/images/getImageCommentById/:imageId", authorization, imageController.getImageCommentById())
+v1.post("/images/saveImage/:imageId", authorization, imageController.saveImage())
+v1.get("/images/getImageSavedById/:imageId", authorization, imageController.getImageSavedById())
+v1.post("/images/commentImage/:imageId", authorization, imageController.commentImage())
 
 //Page quản lý
-v1.get("/users", authorization, userController.getUsers());
-v1.get("/users/getImagesSaved", authorization, userController.getImagesSavedByUserId());
-v1.get("/users/getImagesPosted/:id", authorization, userController.getImagesPostedByUserId());
-v1.delete("/images/:imageId", authorization, imageController.deleteImage())
+v1.get("/users/getUsers", authorization, userController.getUsers());
+v1.get("/users/getImagesSavedByUserId", authorization, userController.getImagesSavedByUserId());
+v1.get("/users/getImagesPostedByUserId", authorization, userController.getImagesPostedByUserId());
+v1.delete("/images/deleteImage/:imageId", authorization, imageController.deleteImage())
 
 //Page thêm ảnh
 v1.post("/upload", authorization, upload.single("file"), imageController.uploadFile())
-v1.post("/images", authorization, imageController.postImage())
-v1.put("/users", authorization, userController.updateUser());
+v1.post("/images/postImage", authorization, imageController.postImage())
+v1.put("/users/updateUser", authorization, userController.updateUser());
 
 module.exports = v1;
