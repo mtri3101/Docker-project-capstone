5edd92511aab825c12033e0ffa3c4bf22d230667
mtri3101 <lamtangminhtri3101@gmail.com>
2023-01-17 08:42:38 +0700

    commit

--- src/controllers/images.controller.js
+++ src/controllers/images.controller.js
@@ -106,7 +106,7 @@ const uploadFile = () => {
         if (!file) {
             next(new AppError(400, "Please update a file"))
         }
-        const tempUrl = `http://localhost:4000/${file.path}`;
+        const tempUrl = `https://docker-project-capstone-production.up.railway.app/${file.path}`;
         const newUrl = tempUrl.replace("static\\", "");
         res.status(200).json(response(newUrl))
     };
